776d2c2dc88752b23c811df78cb8e515595036ad
Sam Minot <sminot@fredhutch.org>
2024-06-07 11:01:28 -0700

    Update routing at base level

--- app.py
+++ app.py
@@ -11,11 +11,23 @@ if __name__ == "__main__":
         # Let the user upload some data to get started
         pages.tables()
 
+    # If no observations have been uploaded
+    elif len(app.list_modalities()) == 0:
+
+        # Let the user upload some data to get started
+        pages.tables()
+
     # If there is data uploaded already
     else:
 
         # If there are figures to show
-        if app.get_views() is not None:
+        if isinstance(app.get_views(), list) and len(app.get_views()) > 0:
 
             # Show the views
             pages.views()
+
+        # If there are no figures to show
+        else:
+
+            # Show the processes
+            pages.processes()
