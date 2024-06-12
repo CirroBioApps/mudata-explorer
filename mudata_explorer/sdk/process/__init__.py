df4d72d26439f6160088d2b75158f97d7247ff77
Sam Minot <sminot@fredhutch.org>
2024-06-12 15:31:10 -0700

    SDK: add summary stats

--- mudata_explorer/sdk/process/__init__.py
+++ mudata_explorer/sdk/process/__init__.py
@@ -3,5 +3,6 @@ from ._decision_tree_classifier import decision_tree_classifier # noqa
 from ._kmeans import kmeans # noqa
 from ._pca import pca # noqa
 from ._random_forest_classifier import random_forest_classifier # noqa
+from ._summary_stats import summary_stats # noqa
 from ._t_test_ind import t_test_ind # noqa
 from ._umap import umap # noqa
