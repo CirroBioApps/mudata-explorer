cfb1f61533c8eadab16c6b58bbfb9267ce3fed05
Sam Minot <sminot@fredhutch.org>
2024-06-12 09:31:10 -0700

    Add process methods to the SDK

--- mudata_explorer/app.py
+++ mudata_explorer/app.py
@@ -10,6 +10,7 @@ from streamlit.delta_generator import DeltaGenerator
 from tempfile import NamedTemporaryFile
 from typing import Any, List, Optional, Tuple, Union, Dict
 from mudata_explorer.helpers import get_view_by_type, all_view_types # noqa
+from mudata_explorer.helpers import get_process_by_type, all_process_types # noqa
 from mudata_explorer.helpers.join_kws import join_kws
 from mudata_explorer.helpers import mudata, plotting, save_load
 from mudata_explorer.base.slice import MuDataSlice
---
+++ mudata_explorer/sdk/process/__init__.py
@@ -0,0 +1,5 @@
+from ._dbscan import dbscan # noqa
+from ._decision_tree_classifier import decision_tree_classifier # noqa
+from ._kmeans import kmeans # noqa
+from ._pca import pca # noqa
+from ._umap import umap # noqa
