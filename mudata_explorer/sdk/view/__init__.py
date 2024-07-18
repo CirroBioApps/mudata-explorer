f7d7d087876f9d715c71de97deefb818f09169af
Sam Minot <sminot@fredhutch.org>
2024-07-18 10:26:28 -0700

    Add table to the SDK

--- mudata_explorer/sdk/view/__init__.py
+++ mudata_explorer/sdk/view/__init__.py
@@ -11,6 +11,7 @@ from ._plotly_scatter import plotly_scatter # noqa
 from ._plotly_scatter_3d import plotly_scatter_3d # noqa
 from ._plotly_scatter_multi import plotly_scatter_multi # noqa
 from ._supporting_figure import supporting_figure # noqa
+from ._table import table # noqa
 from ._group_freq import group_freq # noqa
 from ._markdown import markdown # noqa
 from ._plotly_box import plotly_box # noqa
@@ -24,3 +25,4 @@ from ._plotly_scatter_3d import plotly_scatter_3d # noqa
 from ._plotly_scatter_multi import plotly_scatter_multi # noqa
 from ._seaborn_clustermap import seaborn_clustermap # noqa
 from ._supporting_figure import supporting_figure # noqa
+from ._table import table # noqa
