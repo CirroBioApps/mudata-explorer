c6b15fffd3362118f1f9591e32f2aa2e1f1b21d5
Sam Minot <sminot@gmail.com>
2024-09-10 13:35:30 -0700

    Update run method

--- mudata_explorer/public_data/curatedMetagenomicData/data/run.py
+++ mudata_explorer/public_data/curatedMetagenomicData/data/run.py
@@ -1,93 +1,25 @@
 #!/usr/bin/env python3
 
+from mudata_explorer.parsers import curatedMetagenomicData
+from mudata_explorer.parsers import microbiome
+from mudata_explorer.parsers.microbiome import MicrobiomeParams
 from typing import Tuple
 import json
 from pathlib import Path
 from pandas import read_csv, DataFrame
-from muon import MuData
+from anndata import AnnData
 from mudata_explorer.sdk import io
-from mudata_explorer.parsers.curatedMetagenomicData import read_tsv
-from mudata_explorer.parsers.microbiome import MicrobiomeParams
-from mudata_explorer.parsers.microbiome import _run_processes
-from mudata_explorer.parsers.microbiome import _add_views
 import logging
-from scipy.spatial.distance import pdist
-from scipy.stats import spearmanr
-import umap
 
 logger = logging.getLogger("mudata-curated-metagenomic-data")
 
 
-def umap_params(df, metric="braycurtis", n_components=2):
-    """
-    Select the UMAP parameters which maximize the correlation
-    of the embedded coordinates with the original data
-    """
+def run(adata: AnnData, params: MicrobiomeParams, basename: str):
 
-    # Get the pairwise distances for the input data
-    true_dists = pdist(df, metric=metric)
-
-    dat = []
-    for min_dist in [0.1, 0.2, 0.3, 0.4]:
-        for n_neighbors in [5, 10, 15, 20, 25]:
-            # Run UMAP
-            fit = umap.UMAP(
-                metric=metric,
-                n_neighbors=n_neighbors,
-                min_dist=min_dist,
-                n_components=n_components
-            )
-            umap_coords = fit.fit_transform(df)
-            corr = spearmanr(
-                true_dists,
-                pdist(umap_coords, metric="euclidean")
-            )
-            dat.append(
-                dict(
-                    min_dist=min_dist,
-                    n_neighbors=n_neighbors,
-                    statistic=corr.statistic
-                )
-            )
-
-    dat = DataFrame(dat).sort_values("statistic", ascending=False)
-
-    return dict(
-        metric=metric,
-        n_components=n_components,
-        min_dist=float(dat['min_dist'].values[0]),
-        n_neighbors=int(dat['n_neighbors'].values[0])
-    )
-
-
-def leiden_params():
-    return dict(
-        metric="braycurtis",
-        n_neighbors=15,
-        resolution=1.,
-    )
-
-
-def run(mdata: MuData, config: MicrobiomeParams, basename: str):
-    """
-    1. Read in the TSV file as a DataFrame
-    2. Separate out the metadata columns from the read counts
-        (all of the read counts start with 'k__')
-    3. Transform the read counts to proportions
-    4. Build a MuData object with the metadata and transformed read counts
-    5. Calculate summary metrics for the detected species
-    6. Group the samples by microbiome composition
-    7. Compare those groups to the groups defined in the disease column
-    8. Find the organisms which are most differentially abundant between groups
-    9. Group the samples by those organisms
-    10. Compare those microbiome-based groups to the groups defined
-    """
-
-    # Run the analysis steps
-    _run_processes(mdata, config)
-
-    # Configure the visualizations
-    _add_views(mdata, config)
+    # Run the microbiome analysis
+    mdata = microbiome._parse_adata(adata, groupby_var=True)
+    microbiome._run_processes(mdata, params)
+    microbiome._add_views(mdata, params)
 
     # Save the results
     io.write_h5mu(mdata, basename)
@@ -204,9 +136,6 @@ if __name__ == "__main__":
             else:
                 continue
 
-        # Read the dataset
-        mdata = read_tsv(tsv)
-
         # Read the configuration file
         config_list = [
             MicrobiomeParams(**cfg)
@@ -219,6 +148,15 @@ if __name__ == "__main__":
             if config.compare_by is None:
                 continue
 
+            # Read the dataset as a DataFrame
+            df = read_csv(tsv, sep="\t")
+
+            # Drop any rows which are NaN for config.compare_by
+            df = df.dropna(subset=[config.compare_by])
+
+            # Convert to AnnData
+            adata = curatedMetagenomicData.parse_df(df)
+
             # Name the file for the topic of analysis
             suffix = (
                 config.label
@@ -230,7 +168,7 @@ if __name__ == "__main__":
 
             # Run the analysis
             run(
-                mdata,
+                adata,
                 config,
                 # Name the output based on the column name
                 f"{basename}-{config_ix}-{suffix}"
