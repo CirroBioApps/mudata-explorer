00872356d69e7bb5370354354d1559a595e131cd
Sam Minot <sminot@gmail.com>
2024-10-09 22:26:25 -0700

    Point to the external documentation

--- pages/load_microbiome.py
+++ pages/load_microbiome.py
@@ -1,7 +1,35 @@
+from mudata_explorer.app.sidebar import setup_sidebar
 from mudata_explorer.apps.microbiome import load
-from mudata_explorer.apps.microbiome import menu
+import streamlit as st
 
 # Show the menu at the top
-menu.show_menu(active_page="load_microbiome")
+setup_sidebar(
+    active_page="microbiome",
+    title="Microbiome Report"
+)
+
+# Write the header
+st.markdown('### Microbiome Report')
+
+st.page_link(
+    "https://living-figures.com/post/microbiome-report-intro/",
+    label="What is a Microbiome Report?",
+    icon=":material/info:",
+    disabled=False
+)
+
+st.page_link(
+    "pages/load_microbiome.py",
+    label="Build a Microbiome Report",
+    icon=":material/backup:",
+    disabled=True
+)
+
+st.page_link(
+    "https://living-figures.com/post/microbiome-report-gallery/",
+    label="Browse the Gallery",
+    icon=":material/photo_library:",
+    disabled=False
+)
 
 load.run()
